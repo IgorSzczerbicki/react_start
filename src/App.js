509e979fe92adbd483787fa9fdd25f3e63f95424
Igor Szczerbicki <pl4cek@outlook.com>
2018-07-06 16:41:06 +0200

    Podstawowy komponent funkcjonalny

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<h1>React Start</h1>
+				<Person/>
 			</div>
 
-			// równoważny zapis 
+			// równoważny zapis
 			//
 			// React.createElement('div', null, React.createElement('h1', {className:"App"}, 'React Start Element'))
 		);
