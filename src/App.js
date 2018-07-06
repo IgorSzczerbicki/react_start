fdfcfd0655cc302b14f782a543aff4e88aae3820
Igor Szczerbicki <pl4cek@outlook.com>
2018-07-06 16:19:41 +0200

    JSX vs zapis w React

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>React Start</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>React Start</h1>
+			</div>
+
+			// równoważny zapis 
+			//
+			// React.createElement('div', null, React.createElement('h1', {className:"App"}, 'React Start Element'))
+		);
+	}
 }
 
 export default App;
