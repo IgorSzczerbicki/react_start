e25518f62c0ac92fd49591dbb34df4489193bdc8
Igor Szczerbicki <pl4cek@outlook.com>
2018-07-06 17:08:28 +0200

    Wykorzystanie elementu STATE komponentu

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+	state = {
+		persons: [
+			{name: 'Igor', age: 30},
+			{name: 'Anna', age: 25},
+			{name: 'Marcin', age: 33}
+		]
+	};
 	render() {
 		return (
 			<div className="App">
 				<h1>React Start</h1>
-				<Person name="Igor" age = "30">Moje hobby: sport</Person>
+				<Person name={this.state.persons[0].name} age = {this.state.persons[0].age}>Moje hobby: sport</Person>
 			</div>
 
 			// równoważny zapis
